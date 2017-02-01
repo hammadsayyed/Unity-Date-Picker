881d67798944ca07c5b6e34e37dece981da7beac
Sophia <sophia@Sophias-MacBook-Pro.local>
2017-02-01 13:24:22 +0000

    Initial commit.
    Simple layout with 4 buttons and a input field

---
+++ Assets/Scripts/UpdateInputField.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UpdateInputField : MonoBehaviour {
+
+    public InputField mainInputField;
+    //https://msdn.microsoft.com/en-us/library/bb762911(v=vs.110).aspx
+
+
+    public void UpdateField()
+    {
+        string text = EventSystem.current.currentSelectedGameObject.GetComponent<Button>().GetComponentInChildren<Text>().text;
+        Debug.Log(text);
+        mainInputField.text = text;
+    } 
+}
